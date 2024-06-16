f5d12e529fe8c3620f2b8c50ed9cbd11b5886519
Eduardo navarro <130515812+Dudsnavarro@users.noreply.github.com>
2024-06-16 20:40:25 -0300

    Delete 13-06 directory

--- 13-06/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import type {Metadata} from "next";
-import "./globals.css";
-import PaginaSquirtle from "@/components/PaginaSquirtle/paginaSquirtle";
-
-export default function Home() {
-  return (
-      <div>{<PaginaSquirtle/>}</div>
-  );
-}
--- 13-06/src/components/Header/header-component.tsx
+++
@@ -1,9 +0,0 @@
-import header from './header.module.css'
-
-interface HeaderProp{
-  title: string;
-}
-
-export default function Header(props: HeaderProp) {
-  return (<header className={header.header}><h1 className={header.title}>{props.title}</h1></header>)
-}
